34271eaac7962d690bb64c410f11d616355c5c67
Ziwei Hu <100546637+ZiweiHu1206@users.noreply.github.com>
2022-05-20 16:50:48 -0400

    Add files via upload

---
+++ hw5.ml
@@ -0,0 +1,417 @@
+(* union of list l1 and l2*)
+let union l1 l2 = delete l2 l1 @ l2;;
+
+(* union of free variables in espression es*)
+let union_fvs es =
+  List.fold_left (fun acc exp -> union acc (free_variables exp)) [] es
+;;
+
+(* TODO: Write a good set of tests for unused_vars. *)
+let unused_vars_tests = [
+  (* An example test case.
+     Note that you are *only* required to write tests for Rec, Fn, and Apply!
+  *)
+  (Let ("x", I 1, I 5), ["x"]);
+  (Rec ("x", Int, I 5), ["x"]);
+  (Rec ("x", Int, Var "x"), [] );
+  (Fn ([("x", Int)] , Var "x" ), []);
+  (Fn ([("x", Int)] , Var "y" ), ["x"]);
+  (Fn ([("x", Int); ("y", Int)] , Var "x" ), ["y"]);
+  (Apply (Fn ([("x", Int)], Var "x"), [(I 1)]), []);
+  (Apply (Fn ([("x", Int)], Var "x"), [(I 1); (I 2)]), []);
+]
+;;
+
+
+
+(* TODO: Implement the missing cases of unused_vars. *)
+let rec unused_vars =
+  function
+  | Var _ | I _ | B _ -> []
+  | If (e, e1, e2) -> unused_vars e @ unused_vars e1 @ unused_vars e2
+  | Primop (_, args) ->
+      List.fold_left (fun acc exp -> acc @ unused_vars exp) [] args
+  | Let (x, e1, e2) ->
+      let unused = unused_vars e1 @ unused_vars e2 in
+      if List.mem x (free_variables e2) then
+        unused
+      else
+        x :: unused 
+  | Rec (x, _, e) -> 
+      begin
+        if List.mem x (free_variables e) then
+          unused_vars e
+        else 
+          x :: unused_vars e
+      end 
+  | Fn (xs, e) -> 
+      begin
+        let xs = List.map fst xs in
+        let rec unused input_list unused_list = 
+          match input_list with
+          | [] -> unused_list
+          | hd :: tl -> 
+              if List.mem hd (free_variables e) then
+                unused tl unused_list
+              else 
+                unused tl (hd :: unused_list)
+        in unused xs (unused_vars e)
+      end 
+  | Apply (e, es) -> 
+      begin
+        let rec unused input_list unused_list = 
+          match input_list with
+          | [] -> unused_list
+          | hd :: tl -> 
+              unused tl (List.append (unused_vars hd) unused_list)
+        in unused es (unused_vars e) 
+      end
+;;
+                       
+
+
+
+
+(* TODO: Write a good set of tests for subst. *)
+(* Note: we've added a type annotation here so that the compiler can help
+   you write tests of the correct form. *)
+let subst_tests : (((exp * name) * exp) * exp) list = [
+  (* An example test case. If you have trouble writing test cases of the
+     proper form, you can try copying this one and modifying it.
+     Note that you are *only* required to write tests for Rec, Fn, and Apply!
+  *)
+  (((I 1, "x"), (* [1/x] *)
+    (* let y = 2 in y + x *)
+    Let ("y", I 2, Primop (Plus, [Var "y"; Var "x"]))),
+   (* let y = 2 in y + 1 *)
+   Let ("y", I 2, Primop (Plus, [Var "y"; I 1])));
+  
+  (((I 1, "x"), (* [1/x] *) 
+    Rec ("y", Int, Primop (Plus, [Var "y"; Var "x"]))), 
+   Rec("y", Int, Primop (Plus, [Var "y"; I 1])));
+
+  (((I 1, "x"), (* [1/x] *) 
+    Rec ("x", Int, Primop (Plus, [Var "y"; Var "x"]))), 
+   Rec("x", Int, Primop (Plus, [Var "y"; Var "x"]))); 
+  
+  (((Primop (Plus, [Var "y"; Var "x"]), "y"), 
+    Rec ("x", Int, Primop (Plus, [Var "y"; Var "x"]))), 
+   Rec ("z", Int, Primop (Plus, [Primop (Plus, [Var "y"; Var "x"]); Var "z"])));
+
+  (((I 1, "y"), 
+    Fn ([("y", Int)], Var "y")), 
+   Fn ([("y", Int)], Var "y"));
+  
+  (((I 1, "x"), 
+    Fn ([("y", Int)], Var "x")), 
+   Fn ([("y", Int)], I 1));
+  
+  (((I 1, "x"), 
+    Fn ([("y", Int)], Var "x")), 
+   Fn ([("y", Int)], I 1));
+  
+  (((Primop (Plus, [Var "y"; I 1]), "x"), 
+    Fn ([("y", Int)], Var "x")), 
+   Fn ([("z", Int)], Primop (Plus, [Var "y"; I 1])));
+  
+  (((I 1, "x"), 
+    Apply(Var "y",[Var "x"] )), 
+   Apply(Var "y", [I 1])); 
+  
+  (((I 1, "x"), 
+    Apply(Var "y",[Var "x"; Var "z"] )), 
+   Apply(Var "y", [I 1; Var "z"])); 
+  
+  (((I 1, "x"), 
+    Apply(Var "y",[(Primop (Plus, [Var "y"; Var "x"])); Var "x"])), 
+   Apply(Var "y", [(Primop (Plus, [Var "y"; I 1])); I 1]))
+];;
+
+
+(* TODO: Implement the missing cases of subst. *)
+let rec subst ((e', x) as s) exp =
+  match exp with
+  | Var y ->
+      if x = y then e'
+      else Var y
+  | I n -> I n
+  | B b -> B b
+  | Primop (po, args) -> Primop (po, List.map (subst s) args)
+  | If (e, e1, e2) ->
+      If (subst s e, subst s e1, subst s e2)
+  | Let (y, e1, e2) ->
+      let e1' = subst s e1 in
+      if y = x then
+        Let (y, e1', e2)
+      else
+        let (y, e2) =
+          if List.mem y (free_variables e') then
+            rename y e2
+          else
+            (y, e2)
+        in
+        Let (y, e1', subst s e2) 
+  | Rec (y, t, e) -> 
+      begin 
+        if y = x then
+          Rec (y, t, e)
+        else
+          let (y, e) =
+            if List.mem y (free_variables e') then
+              rename y e 
+            else
+              (y, e) 
+          in Rec (y, t, subst s e) 
+      end 
+  | Fn (xs, e) -> 
+      begin
+        let input_list = List.map fst xs in 
+        if List.mem x input_list then
+          Fn (xs, e)
+        else
+          let sub_e = subst s e in
+          let (name', exp') = rename_all input_list e in
+          if List.exists (fun x -> 
+              List.mem x (free_variables e')) input_list then 
+            Fn ((List.map2 (fun x (a', t) -> (x, t)) name' xs), (subst s exp'))
+          else
+            Fn (xs, sub_e) 
+      end
+  | Apply (e, es) -> 
+      begin
+        let sub_e = subst s e in 
+        Apply (sub_e, List.map (subst s) es)
+      end
+      
+and rename x e =
+  let x' = freshVar x in
+  (x', subst (Var x', x) e)
+
+and rename_all names exp =
+  List.fold_right
+    (fun name (names, exp) ->
+       let (name', exp') = rename name exp in
+       (name' :: names, exp'))
+    names
+    ([], exp)
+;;
+    
+
+
+
+
+(* Applying a list of substitutions to an expression, leftmost first *)
+let subst_list subs exp =
+  List.fold_left (fun exp sub -> subst sub exp) exp subs
+
+    
+(* TODO: Write a good set of tests for eval. *)
+let eval_tests = [
+  (* An example test case.
+     Note that you are *only* required to write tests for Rec and Apply!
+  *)
+  (Let ("x", I 1, Primop (Plus, [Var "x"; I 5])), I 6);
+  (Rec ("x", Int, Primop(Plus, [I 1; I 5])), I 6); 
+  (Rec ("x", Int, Let ("y", I 1, Primop (Plus, [Var "y"; I 5]))), I 6);
+  (Apply (Fn ([], I 1), []), I 1);
+  (Apply (Fn ([("x", Int)], Var "x"), [(I 1)]), I 1 ); 
+  (Apply (Fn ([("x", Int);("y", Int)],
+              Primop(Plus, [Var "x"; Var "y"])), [(I 1);(I 1)]), I 2);
+  (Apply (If (B true, Fn ([], I 1),  I 2), []), I 1)
+]
+
+
+(* TODO: Implement the missing cases of eval. *)
+let rec eval exp =
+  match exp with
+  (* Values evaluate to themselves *)
+  | I _ -> exp
+  | B _ -> exp
+  | Fn _ -> exp
+
+  (* This evaluator is _not_ environment-based. Variables should never
+     appear during evaluation since they should be substituted away when
+     eliminating binding constructs, e.g. function applications and lets.
+     Therefore, if we encounter a variable, we raise an error.
+*)
+  | Var x -> raise (Stuck (Free_variable x))
+
+  (* Primitive operations: +, -, *, <, = *)
+  | Primop (po, args) ->
+      let args = List.map eval args in
+      begin
+        match eval_op po args with
+        | None -> raise (Stuck Bad_primop_args)
+        | Some v -> v
+      end
+
+  | If (e, e1, e2) ->
+      begin
+        match eval e with
+        | B true -> eval e1
+        | B false -> eval e2
+        | _ -> raise (Stuck If_non_true_false)
+      end
+
+  | Let (x, e1, e2) ->
+      let e1 = eval e1 in
+      eval (subst (e1, x) e2)
+
+  | Rec (f, _, e) -> 
+      begin
+        let s = (exp, f) in 
+        let free_var = free_variables e in
+        if List.mem f (free_var) then
+          eval (subst s (eval e))
+        else
+          eval e
+      end 
+      
+  | Apply (e, es) -> 
+      begin
+        match eval e with
+        | Fn (input_list, exp') ->
+            if List.length input_list = List.length es then
+              let subs' = List.combine es (List.map fst input_list) in
+              eval (subst_list subs' exp')
+            else 
+              raise (Stuck Arity_mismatch) 
+        | _ -> raise (Stuck Apply_non_fn)
+        
+      end
+
+  
+
+
+
+(* TODO: Write a good set of tests for infer. *)
+let infer_tests = [
+  (* An example test case.
+     Note that you are *only* required to write tests for Rec, Fn, and Apply!
+  *)
+  (([("x", Int)], Var "x"), Int);
+  (([], Rec("x", Int, Var "x")), Int); 
+  (([], ex1), Arrow ([Int; Int], Int)); 
+  (([], ex2), Arrow ([], Bool)); 
+  (([], Fn([("x", Int)], I 1)), Arrow([Int], Int));
+  (([], Fn([("x", Int);("y", Int)], I 1)), Arrow([Int; Int], Int)); 
+  (([], Apply(Fn ([], I 1),[])), Int);
+  (([], Apply(Fn ([("x", Int)], Var "x"),[I 1])), Int);
+  (([("x", Arrow ([Int; Int], Int))], 
+    (Apply (Var "x", [I 1; I 5]))), Int) 
+]
+
+(* TODO: Implement the missing cases of infer. *)
+let rec infer ctx e =
+  match e with
+  | Var x ->
+      begin
+        try lookup x ctx
+        with Not_found -> raise (TypeError (Free_variable x))
+      end
+  | I _ -> Int
+  | B _ -> Bool
+
+  | Primop (po, exps) ->
+      let (domain, range) = primopType po in
+      check ctx exps domain range
+
+  | If (e, e1, e2) ->
+      begin
+        match infer ctx e with
+        | Bool ->
+            let t1 = infer ctx e1 in
+            let t2 = infer ctx e2 in
+            if t1 = t2 then t1
+            else type_mismatch t1 t2
+        | t -> type_mismatch Bool t
+      end
+
+  | Let (x, e1, e2) ->
+      let t1 = infer ctx e1 in
+      infer (extend ctx (x, t1)) e2
+
+  | Rec (f, t, e) -> 
+      begin
+        let t' = infer (extend ctx (f, t)) e in
+        if t = t' then t
+        else 
+          type_mismatch t t' 
+      end 
+
+  | Fn (xs, e) -> 
+      begin
+        let tp_list = List.map snd xs in
+        Arrow (tp_list, infer (extend_list ctx xs) e) 
+      end
+  
+  | Apply (e, es) -> 
+      begin
+        let t' = infer ctx e in 
+        match t' with
+        | Arrow (tp_list, tp) -> check ctx es tp_list tp
+        | _ -> raise (TypeError (Apply_non_arrow t'))
+      end
+  
+      
+and check ctx exps tps result =
+  match exps, tps with
+  | [], [] -> result
+  | e :: es, t :: ts ->
+      let t' = infer ctx e in
+      if t = t' then check ctx es ts result
+      else type_mismatch t t'
+  | _ -> raise (TypeError Arity_mismatch)
+
+           
+
+
+
+           
+
+(* TODO: Implement type unification. *)
+let unify : utp -> utp -> utp UTVarMap.t =
+  let rec unify (substitution : utp UTVarMap.t) (t1 : utp) (t2 : utp) : utp UTVarMap.t =
+    match t1, t2 with
+    (* Unifying identical concrete types does nothing *)
+    | UInt, UInt
+    | UBool, UBool -> substitution
+    | UTVar a, UTVar a' when a = a' -> substitution
+
+    (* For type constructors, recursively unify the parts *)
+    | UArrow (t1, t1'), UArrow (t2, t2') ->
+        begin
+          unify (unify substitution t1 t2) t1' t2' 
+        end
+
+    | UTVar a, _ -> unifyVar substitution a t2
+    | _, UTVar b -> unifyVar substitution b t1
+    (* All other cases are mismatched types. *)
+    | _, _ -> unif_error @@ UnifMismatch (t1, t2)
+  
+  (* Unify a variable with a type *)
+  and unifyVar (substitution : utp UTVarMap.t) (a : string) (t : utp) : utp UTVarMap.t =
+    let rec occurs : utp -> bool = function
+      | UInt | UBool -> false
+      | UArrow (t1, t2) -> occurs t1 || occurs t2
+      | UTVar b ->
+          if a = b
+          then true
+          else
+            match UTVarMap.find_opt b substitution with
+            | None -> false
+            | Some t' -> occurs t'
+    in 
+    begin
+      if occurs t then
+        unif_error UnifOccursCheckFails
+      else
+        match UTVarMap.find_opt a substitution with
+        | None -> UTVarMap.add a t substitution
+        | Some _ -> unif_error UnifOccursCheckFails
+      
+      
+    end 
+    
+
+  in fun t1 t2 -> unify UTVarMap.empty t1 t2
